dd30b1b4d2135402e2c3dc927c790fba6157147c
Spencer <spencer.michael.q@gmail.com>
2017-06-26 18:44:46 +0800

    .

--- www.practicepython.org/2-oddeven.py
+++ www.practicepython.org/2-oddeven.py
@@ -2,3 +2,19 @@
 # print out an appropriate message to the user.
 # Hint: how does an even / odd number react differently when divided by 2?
 
+# http://www.practicepython.org/exercise/2014/02/05/02-odd-or-even.html
+
+while True:
+    inp = input("Enter an integer, type x to close: ")
+
+    if inp == 'x':
+        break
+
+    elif int(inp)%2 == 0:
+        print('number is even')
+
+    elif int(inp)%2 != 0:
+        print("number is odd")
+
+    else:
+        print('Please enter valid input')
