e72081a1d299a7fae30108be2fc913686771a725
Spencer <spencer.michael.q@gmail.com>
2017-06-26 19:24:24 +0800

    .

--- www.practicepython.org/4-divisors_of_a_number.py
+++ www.practicepython.org/4-divisors_of_a_number.py
@@ -2,14 +2,16 @@
 # list of all the divisors of that number. (If you don’t know what a divisor is, it is a number that divides
 # evenly into another number. For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
 
-while True:
-    inp = input("Enter a number, type 'x' to quit: ")
+__author__ = 'SpencerMAQ'
 
-    if inp == 'x':
+while True:
+    inp = int(input("Enter a number, type '0' to quit: "))
+
+    if inp == 0:
         break
 
     else:
         # print(12%3)
         # from range 2 because 1 is always a divisor //  to inp+1 = inp itself
-        list = [x for x in range(2, int(inp)+1) if int(inp)%x == 0]
-        print(list)
+        divisorList = [x for x in range(2, inp + 1) if inp%x == 0]
+        print(divisorList)
