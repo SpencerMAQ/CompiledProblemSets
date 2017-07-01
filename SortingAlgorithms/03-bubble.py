8d2abd2281a6195d2c594f6061084fec32b7b748
Spencer <spencer.michael.q@gmail.com>
2017-07-01 17:15:58 +0800

    .

--- SortingAlgorithms/03-bubble.py
+++ SortingAlgorithms/03-bubble.py
@@ -1,4 +1,44 @@
 a = [7, 8, 5, 4, 9, 2, 5, 5, 10, 3, 1]
 
-for i in range(len(a)-1):
-    
+_sorted = False
+# ctr = 0
+# for i in range(len(a)-1):
+#     ctr = 0
+#     while _sorted == False:
+#         if a[i] > a[i+1]:
+#             a[i], a[i+1] = a[i+1], a[i]
+#             ctr += 1
+#
+#             if ctr > 0:
+#                 _sorted = False
+#             else:
+#                 _sorted = True
+#
+# print(a)
+
+a = [7, 8, 5, 4, 9, 2]
+
+sorted_list=['True','True','True']
+print(all(['True','True','True']) == 'True')
+
+
+while any(sorted_list)
+# while True:
+#
+#     sorted_list = []
+#
+#     if all(sorted_list) == 'True':
+#         break
+#
+#     for i in range(len(a)-1):
+#         if a[i] > a[i+1]:
+#             a[i], a[i + 1] = a[i + 1], a[i]
+#             sorted_list.append('False')
+#
+#         else:
+#             sorted_list.append('True')
+#
+#
+#
+#
+# print(a)
